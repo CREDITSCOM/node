3b25904c640927b49fccc3d37bc6b285f0b89473
0xAAE <avramenko.a@gmail.com>
2018-09-09 17:53:02 +0300

    Отказ от "перетяжеленной" версии, проще - лучше

--- lib/include/RunAfter.h
+++ lib/include/RunAfter.h
@@ -5,17 +5,17 @@
  * 			The class instance is a special object to call any method after some delay applying desired LaunchScheme (see below)
  *
  * 			Samples:
- * 			1. Declare a class member to call argless method:  
- * 				a) declare and implement a method  
+ * 			1. Declare a class member to call argless method:
+ * 				a) declare and implement a method
  *	@code		void MethodName();
  * 				b) declare a method caller
  *	@code		RunAfterEx<CustomProc> method_name_caller;
- *				c) init caller in constructor like this  
+ *				c) init caller in constructor like this
  *	@code		method_name_caller([this]() { MethodName(); }, "MethodName()")
  *				d) call MethodName() once with delay of 1 second
  *	@code		method_name_caller.Schedule( std::chrono::milliseconds(1000), LaunchScheme::single )
  *				d) call MethodName() periodically with delay of 200 milliseconds
- *	@code		method_name_caller.Schedule( std::chrono::milliseconds(200), LaunchScheme::periodic )
+ *	@code		method_name_caller.Schedule( std::chrono::milliseconds(200), LaunchScheme::periodic_single )
  *				e) stop periodic calling or cancel scheduled call
  *	@code		method_name_caller.Cancel();
  * 			2. Declare a class member to call 2-args method:
@@ -36,23 +36,23 @@
  *	@code		method_name_caller.Cancel();
  */
 
-/**
- * Inspired by:
- *	void runAfter(const std::chrono::milliseconds& ms, std::function<void()> cb, const char * msg)
- *	{
- *		const auto tp = std::chrono::system_clock::now() + ms;
- *		std::thread tr([tp, cb, msg]() {
- *			std::this_thread::sleep_until(tp);
- *			LOG_WARN("Inserting callback " << msg);
- *			CallsQueue::instance().insert(cb);
- *		});
- *		tr.detach();
- *	}
-*/
+ /**
+  * Inspired by:
+  *	void runAfter(const std::chrono::milliseconds& ms, std::function<void()> cb, const char * msg)
+  *	{
+  *		const auto tp = std::chrono::system_clock::now() + ms;
+  *		std::thread tr([tp, cb, msg]() {
+  *			std::this_thread::sleep_until(tp);
+  *			LOG_WARN("Inserting callback " << msg);
+  *			CallsQueue::instance().insert(cb);
+  *		});
+  *		tr.detach();
+  *	}
+ */
 
 #pragma once
 
-// Uncomment to activate detailed log using timer_service (header 'timer_service.h' required>
+ // Uncomment to activate detailed log using timer_service (header 'timer_service.h' required>
 #define TIMER_SERVICE_LOG
 
 #include <chrono>
@@ -83,10 +83,10 @@ enum class LaunchScheme {
 
 
 /** @brief	The custom procedure */
-using CustomProc = std::function<void()>;
+using CustomProc = std::function<void ()>;
 
 /** @brief	The custom procedure int argument */
-using CustomProcIntArg = std::function<void(int)>;
+using CustomProcIntArg = std::function<void (int)>;
 
 /**
  * @class	RunAfterEx
@@ -105,8 +105,8 @@ class RunAfterEx
 {
 public:
 
-	RunAfterEx() = delete;
-	RunAfterEx(const RunAfterEx&) = delete;
+	RunAfterEx () = delete;
+	RunAfterEx (const RunAfterEx&) = delete;
 	void operator =(const RunAfterEx&) = delete;
 
 	/**
@@ -121,9 +121,9 @@ public:
 	 * @param [in,out]	comment	The comment.
 	 */
 
-	RunAfterEx(TProc&& proc, std::string&& comment)
-		: _proc(std::forward<TProc>(proc))
-		, _comment(std::forward<std::string>(comment))
+	RunAfterEx (TProc&& proc, std::string&& comment)
+		: _proc (std::forward<TProc> (proc))
+		, _comment (std::forward<std::string> (comment))
 	{
 	}
 
@@ -142,86 +142,59 @@ public:
 	 */
 
 	template<typename... Args>
-	void Schedule(TRes&& wait_for, LaunchScheme scheme, Args... args)
+	void Schedule (TRes&& wait_for, LaunchScheme scheme, Args... args)
 	{
 
 #if defined(TIMER_SERVICE_LOG)
 		std::ostringstream os;
-		os << "RunAfterEx: schedule (" << wait_for.count() << ") " << _comment;
-		timer_service.Mark(os.str(), -1);
+		os << "RunAfterEx: schedule (" << wait_for.count () << ") " << _comment;
+		timer_service.Mark (os.str (), -1);
 #endif
 
 		// test has not already launched in single cases
 		if (_launched)
 		{
 #if defined(TIMER_SERVICE_LOG)
-				timer_service.Mark(os.str() + " canceled (already running)", -1);
+				timer_service.Mark (os.str () + " canceled (already running)", -1);
 #endif
-			return;
+				return;
 		}
 
+		_cancel = false;
 		_launched = true;
+		std::thread ([this, wait_for, scheme, args...] () {
 
-		// obtain launch count depending on scheme (-1 - forever)
-		{
-			Lock l (_mtx);
+			std::this_thread::sleep_for (wait_for);
+
+			// test cancel condition after waiting finished
+			if (_cancel) {
+				_launched = false;
+#if defined(TIMER_SERVICE_LOG)
+				std::ostringstream os;
+				os << "RunAfterEx: schedule (" << wait_for.count () << ") " << _comment;
+				timer_service.Mark (os.str () + " canceled (while waiting)", -1);
+#endif
+				return;
+			}
+
 			switch (scheme) {
 			case LaunchScheme::single:
-				_remains = 1;
+				ExecuteProc (std::forward<TProc> (_proc), args...);
 				break;
 			case LaunchScheme::periodic:
-				_remains = -1;
-				break;
-			default:
-				_remains = 0;
+				// launch periodically until external stop
+				do {
+					ExecuteProc (std::forward<TProc> (_proc), args...);
+					if (_cancel) {
+						break;
+					}
+					std::this_thread::sleep_for (wait_for);
+					// test stop condition
+				} while (!_cancel);
 				break;
 			}
-		}
-
-		std::thread([this, wait_for, scheme, args...]() {
-
-			std::this_thread::sleep_for(wait_for);
-			
-			// launch until _remains != 0
-			do {
-				// test stop condition
-				if (HandleStopCondition ()) {
-					break;
-				}
-				ExecuteProc (std::forward<TProc> (_proc), args...);
-				if (HandleStopCondition ()) {
-					break;
-				}
-				std::this_thread::sleep_for (wait_for);
-			} while (true);
-
 			_launched = false;
-#if defined(TIMER_SERVICE_LOG)
-			std::ostringstream os2;
-			os2 << "RunAfterEx: " << _comment << " is finished";
-			timer_service.Mark (os2.str (), -1);
-#endif
-		}).detach();
-	}
-
-	/**
-	 * @fn	void CancelAfter()
-	 *
-	 * @brief	Cancels this object
-	 *
-	 * @author	User
-	 * @date	07.09.2018
-	 */
-
-	void CancelAfter( int count_calls )
-	{
-		Lock l (_mtx);
-		if (count_calls < 0 || ( _remains > 0 && _remains <= count_calls ) ) {
-			// should not schedule periodic calls using CancelAfter()
-			// should not re-schedule more calls using CancelAfter()
-			return;
-		}
-		_remains = count_calls;
+		}).detach ();
 	}
 
 	/**
@@ -233,10 +206,9 @@ public:
 	 * @date	07.09.2018
 	 */
 
-	void Cancel()
+	void Cancel ()
 	{
-		Lock l (_mtx);
-		_remains = 0;
+		_cancel = true;
 	}
 
 	bool IsScheduled () const
@@ -248,20 +220,18 @@ private:
 
 	TProc _proc;
 	std::string _comment;
-	std::atomic_bool _launched{ false };
-	int _remains = 0;
-	std::mutex _mtx;
-	using Lock = std::lock_guard<std::mutex>;
+	std::atomic_bool _launched { false };
+	std::atomic_bool _cancel { false };
 
 	// Special 1-arg variant writes more detailed log (with argument value)
 #if defined(TIMER_SERVICE_LOG)
-	void ExecuteProc(CustomProcIntArg&& proc, int arg)
+	void ExecuteProc (CustomProcIntArg&& proc, int arg)
 	{
 		std::ostringstream os;
 		os << "RunAfterEx: launching " << _comment;
-		timer_service.Mark(os.str(), arg);
+		timer_service.Mark (os.str (), arg);
 		//auto _p = std::forward<TProc>(proc);
-		CallsQueue::instance().insert([proc, arg]() { proc(arg); });
+		CallsQueue::instance ().insert ([proc, arg] () { proc (arg); });
 	}
 #endif
 
@@ -279,25 +249,13 @@ private:
 	 */
 
 	template<typename... Args>
-	void ExecuteProc(std::function<void(Args...)>&& proc, Args... args)
+	void ExecuteProc (std::function<void (Args...)>&& proc, Args... args)
 	{
 #if defined(TIMER_SERVICE_LOG)
 		std::ostringstream os;
 		os << "RunAfterEx: launching " << _comment;
-		timer_service.Mark(os.str());
+		timer_service.Mark (os.str ());
 #endif
-		CallsQueue::instance().insert([proc, args...]() { proc(args...); });
-	}
-
-	bool HandleStopCondition ()
-	{
-		Lock l (_mtx);
-		if (_remains > 0) {
-			_remains--;
-		}
-		if (_remains == 0) {
-			return true;
-		}
-		return false;
+		CallsQueue::instance ().insert ([proc, args...] () { proc (args...); });
 	}
 };
