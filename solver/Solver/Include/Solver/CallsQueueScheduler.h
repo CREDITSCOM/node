cae616f3ca456bb461ad1a16020cdd586b9947cd
avramenko_a <aav@credits.com>
2018-09-17 14:38:25 +0300

    CallsQueueScheduler debugged & tested

--- solver/Solver/Include/Solver/CallsQueueScheduler.h
+++ solver/Solver/Include/Solver/CallsQueueScheduler.h
@@ -12,12 +12,12 @@ class CallsQueueScheduler
 {
 public:
  
-    enum class LaunchScheme {
+    enum class Launch {
 
-        ///< An enum constant representing the single option: launch once, cancel if previous schedule call still is not done
-        single,
+        ///< An enum constant representing the once option: launch once, cancel if previous schedule call still is not done
+        once,
 
-        ///< An enum constant representing the periodic single option: launch periodically, schedule call cancels if already running one cycle
+        ///< An enum constant representing the periodic option: launch periodically, schedule call cancels if already running one cycle
         periodic
     };
 
@@ -25,6 +25,15 @@ public:
     using ProcType = std::function<void()>;
 
     constexpr static uintptr_t no_id = 0;
+
+    /**
+     * @fn  CallsQueueScheduler::CallsQueueScheduler()
+     *
+     * @brief   Default constructor
+     *
+     * @author  aae
+     * @date    17.09.2018
+     */
 
     CallsQueueScheduler()
         :_queue( compare )
@@ -34,44 +43,140 @@ public:
     CallsQueueScheduler(const CallsQueueScheduler&) = delete;
     CallsQueueScheduler& operator =(const CallsQueueScheduler&) = delete;
 
+    /**
+     * @fn  void CallsQueueScheduler::Run();
+     *
+     * @brief   Runs scheduler in separate thread, is optional for call. Would be automatically called by the first Insert() operation
+     *
+     * @author  aae
+     * @date    17.09.2018
+     */
+
     void Run();
+
+    /**
+     * @fn  void CallsQueueScheduler::Stop();
+     *
+     * @brief   Stops this object by clearing the queue and waits for worker thread to stop
+     *
+     * @author  aae
+     * @date    17.09.2018
+     */
 
     void Stop();
 
-    uintptr_t Insert(ClockType::duration wait_for, ProcType proc, LaunchScheme scheme, const std::string& comment);
+    /**
+     * @fn  uintptr_t CallsQueueScheduler::Insert(ClockType::duration wait_for, const ProcType& proc, Launch scheme);
+     *
+     * @brief   Inserts new call into queue according to wait_for parameter. Do check before insert to avoid queuing of duplicated calls
+     *
+     * @author  aae
+     * @date    17.09.2018
+     *
+     * @param   wait_for    Time to wait before do call a procedure.
+     * @param   proc        The procedure to be scheduled for call.
+     * @param   scheme      The scheme: once - do one call, periodic - repeat calls every wait_for period.
+     *
+     * @return  An id that can be used in future to remove scheduled call from queue if any.
+     */
+
+    uintptr_t Insert(ClockType::duration wait_for, const ProcType& proc, Launch scheme/*, const std::string& comment*/);
+
+    /**
+     * @fn  bool CallsQueueScheduler::Remove(uintptr_t id);
+     *
+     * @brief   Removes the scheduled call idenified by id. 
+     *
+     * @author  aae
+     * @date    17.09.2018
+     *
+     * @param   id  The identifier of call to remove. The id must be obtained by previous call to Insert()
+     *
+     * @return  True if it succeeds, false if it fails.
+     */
 
     bool Remove(uintptr_t id);
+
+    /**
+     * @fn  void CallsQueueScheduler::Clear();
+     *
+     * @brief   Clears the queue of scheduled calls
+     *
+     * @author  aae
+     * @date    17.09.2018
+     */
+
+    void Clear();
+
+    /**
+     * @fn  uint32_t CallsQueueScheduler::TotalExecutedCalls() const;
+     *
+     * @brief   Total executed calls
+     *
+     * @author  aae
+     * @date    17.09.2018
+     *
+     * @return  The total number of executed calls during work.
+     */
 
     uint32_t TotalExecutedCalls() const;
 
 private:
 
+    /**
+     * @struct  Context
+     *
+     * @brief   Stores all info to call, re-schedule and cancel further calls in queue.
+     *
+     * @author  aae
+     * @date    17.09.2018
+     */
+
     struct Context
     {
-        uintptr_t id;
-        ClockType::time_point tp;
-        //LaunchScheme scheme;
-        long long dt;
-        ProcType proc;
-        std::string comment;
 
+        /** @brief   The identifier: lets find item in queue (e.g. for remove) */
+        uintptr_t id;
+
+        /** @brief   The time point for scheduled execution */
+        ClockType::time_point tp;
+
+        /** @brief   The delta - time period for periodic calls */
+        long long dt;
+
+        /** @brief   The procedure to call*/
+        ProcType proc;
+        
+        // not used, useful for debug purpose
+        //std::string comment;
+
+        // support for std::find() by id
         bool operator==(uintptr_t rhs) const
         {
             return id == rhs;
         }
     };
 
+    // used for sort items in _queue by time to call
     std::function<bool(const Context& lhs, const Context& rhs)> compare =
         [](const Context& lhs, const Context& rhs) { return lhs.tp < rhs.tp; };
-    //std::priority_queue<Context, std::vector<Context>, decltype(compare)> _queue;
-    std::set<Context, decltype(compare)> _queue;
+
+    // container requirements: auto sort by tp, search by id
+    std::multiset<Context, decltype(compare)> _queue;
+    // sync access to _queue
     std::mutex _mtx_queue;
 
-    std::condition_variable _signal;
+    // process _queue and puts on time calls into CallsQueue::instance() object
     std::thread _worker;
-    std::mutex _mtx_signal;
 
+    // signals to _worker thread that _queue was updated
+    std::condition_variable _signal;
+    // need by _signal
+    std::mutex _mtx_signal;
+    // avoids "false" signals
     bool _flag { false };
+
+    // flag to stop _worker thread
     std::atomic_bool _stop { false };
 
     // statistics
--- solver/Solver/Src/Solver/CallsQueueScheduler.cpp
+++ solver/Solver/Src/Solver/CallsQueueScheduler.cpp
@@ -49,25 +49,27 @@ void CallsQueueScheduler::Run()
             run.id = no_id;
             {
                 std::lock_guard<std::mutex> lque(_mtx_queue);
-                if(!_queue.empty()) {
-                    if((_queue.cbegin()->tp - ClockType::now()).count() < min_wait_for) {
-                        run = *_queue.cbegin();
-                        _queue.erase(_queue.cbegin());
-                        if(run.dt > 0) {
-                            // LaunchScheme::periodic -> schedule next item
-                            if(no_id == Insert(std::chrono::milliseconds(run.dt), run.proc, LaunchScheme::periodic, run.comment)) {
-                                // periodic calls aborted due to unexpected problem!
-                            }
+                // execute calls until wait time >= min_wait_for
+                while(!_queue.empty()) {
+                    if((_queue.cbegin()->tp - ClockType::now()).count() >= min_wait_for) {
+                        break;
+                    }
+                    run = *_queue.cbegin();
+                    _queue.erase(_queue.cbegin());
+                    ProcType proc = run.proc;
+                    CallsQueue::instance().insert([proc]() {
+                        proc();
+                    });
+                    _count_total.fetch_add(1);
+                    // Launch::periodic -> schedule next item
+                    if(run.dt > 0) {
+                        run.tp += std::chrono::milliseconds(run.dt);
+                        auto pos = _queue.insert(run);
+                        if(pos == _queue.end()) {
+                            // periodic calls aborted due to unexpected problem!
                         }
                     }
                 }
-            }
-            if(run.id != no_id) {
-                ProcType proc = _queue.cbegin()->proc;
-                CallsQueue::instance().insert([proc]() {
-                    proc();
-                });
-                _count_total.fetch_add(1);
             }
         }
     });
@@ -75,17 +77,21 @@ void CallsQueueScheduler::Run()
 
 void CallsQueueScheduler::Stop()
 {
+    Clear();
     _stop = true;
-    _flag = true;
     // awake worker thread if it sleeps
+    _flag = true;
     _signal.notify_one();
     if(_worker.joinable()) {
         _worker.join();
     }
 }
 
-uintptr_t CallsQueueScheduler::Insert(ClockType::duration wait_for, ProcType proc, LaunchScheme scheme, const std::string& comment)
+uintptr_t CallsQueueScheduler::Insert(ClockType::duration wait_for, const ProcType& proc, Launch scheme/*, const std::string& comment*/)
 {
+    if(!_worker.joinable()) {
+        Run();
+    }
     uintptr_t id = (uintptr_t) &proc;
     {
         std::lock_guard<std::mutex> l(_mtx_queue);
@@ -97,15 +103,16 @@ uintptr_t CallsQueueScheduler::Insert(ClockType::duration wait_for, ProcType pro
         auto result = _queue.insert(CallsQueueScheduler::Context {
             id,
             ClockType::now() + wait_for,
-            ( scheme == LaunchScheme::single ? 0 : std::chrono::duration_cast<std::chrono::milliseconds>(wait_for).count() ),
-            std::move(proc),
-            std::move(comment)
+            ( scheme == Launch::once ? 0 : std::chrono::duration_cast<std::chrono::milliseconds>(wait_for).count() ),
+            std::move(proc)
+            //, std::move(comment)
         });
-        if(!result.second) {
+        if(result == _queue.end()) {
             return no_id;
         }
     }
     // awake worker thread to re-schedule its waiting
+    _flag = true;
     _signal.notify_one();
     return id;
 }
@@ -121,8 +128,20 @@ bool CallsQueueScheduler::Remove(uintptr_t id)
         _queue.erase(it);
     }
     // awake worker thread to re-schedule its waiting
+    _flag = true;
     _signal.notify_one();
     return true;
+}
+
+void CallsQueueScheduler::Clear()
+{
+    {
+        std::lock_guard<std::mutex> l(_mtx_queue);
+        _queue.clear(); 
+    }
+    // awake worker thread to re-schedule its waiting
+    _flag = true;
+    _signal.notify_one();
 }
 
 uint32_t CallsQueueScheduler::TotalExecutedCalls() const
