3d4a6ef0e0b2a32d0a8d716364c47ea8d84c496b
andrewz <zabavnikov@gmail.com>
2018-09-25 11:30:47 +0300

    NODE-112: NODE-129: Port of new stats and API to cs_stable without any brain usage

--- api/include/APIHandler.h
+++ api/include/APIHandler.h
@@ -241,3 +241,36 @@ class SequentialProcessorFactory : public ::apache::thrift::TProcessorFactory
 
 api::SealedTransaction
 convertTransaction(const csdb::Transaction& transaction);
+
+template<typename T>
+T
+deserialize(std::string&& s)
+{
+
+  using namespace ::apache;
+
+  // https://stackoverflow.com/a/16261758/2016154
+  static_assert(
+    CHAR_BIT == 8 && std::is_same<std::uint8_t, unsigned char>::value,
+    "This code requires std::uint8_t to be implemented as unsigned char.");
+
+  auto buffer = thrift::stdcxx::make_shared<thrift::transport::TMemoryBuffer>(
+    reinterpret_cast<uint8_t*>(&(s[0])), (uint32_t)s.size());
+  thrift::protocol::TBinaryProtocol proto(buffer);
+  T sc;
+  sc.read(&proto);
+  return sc;
+}
+
+template<typename T>
+std::string
+serialize(const T& sc)
+{
+  auto buffer = thrift::stdcxx::make_shared<thrift::transport::TMemoryBuffer>();
+  thrift::protocol::TBinaryProtocol proto(buffer);
+  sc.write(&proto);
+  return buffer->getBufferAsString();
+}
+
+bool
+is_deploy_transaction(const csdb::Transaction& tr);
