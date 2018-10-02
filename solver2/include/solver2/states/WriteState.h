49ec4dccf28694e57ae4019dfa8005bf28abbaf3
avramenko_a <aav@credits.com>
2018-10-02 10:54:55 +0300

    Re-format comments

--- solver2/include/solver2/states/WriteState.h
+++ solver2/include/solver2/states/WriteState.h
@@ -4,11 +4,18 @@
 
 namespace slv2
 {
-    /// <summary>   A write node state. This class cannot be inherited. </summary>
-    ///
-    /// <remarks>   Aae, 30.09.2018. </remarks>
-    ///
-    /// <seealso cref="T:DefaultStateBehavior"/>
+    /**
+     * @class   WriteState
+     *
+     * @brief   A write node state. This class cannot be inherited.
+     *
+     * @author  aae
+     * @date    02.10.2018
+     *
+     * @sa  T:DefaultStateBehavior  
+     *
+     * ### remarks  Aae, 30.09.2018.
+     */
 
     class WriteState final : public DefaultStateBehavior
     {
@@ -19,17 +26,42 @@ namespace slv2
 
         void on(SolverContext& context) override;
 
+        /**
+         * @fn  Result final::onHash(SolverContext& context, const Hash& hash, const PublicKey& sender) override;
+         *
+         * @brief   Executes the hash action
+         *
+         * @author  aae
+         * @date    02.10.2018
+         *
+         * @param [in,out]  context The context.
+         * @param           hash    The hash.
+         * @param           sender  The sender.
+         *
+         * @return  A Result.
+         */
+
         Result onHash(SolverContext& context, const Hash& hash, const PublicKey& sender) override;
 
-        /// <summary>   Override DefaultStateBehavior's method to ignore blocks received </summary>
-        ///
-        /// <remarks>   Aae, 30.09.2018. </remarks>
-        ///
-        /// <param name="context">  not used. </param>
-        /// <param name="pool">     not used. </param>
-        /// <param name="sender">   not used. </param>
-        ///
-        /// <returns>   A Result::Ignore value </returns>
+        /**
+         * @fn  Result final::onBlock(SolverContext& , csdb::Pool& , const PublicKey& ) override
+         *
+         * @brief   Override DefaultStateBehavior's method to ignore blocks received
+         *
+         * @author  aae
+         * @date    02.10.2018
+         *
+         * @param [in,out]  parameter1  The first parameter.
+         * @param [in,out]  parameter2  The second parameter.
+         * @param           parameter3  The third parameter.
+         *
+         * @return  A Result::Ignore value.
+         *
+         * ### remarks  Aae, 30.09.2018.
+         * ### param            context not used.
+         * ### param            pool    not used.
+         * ### param            sender  not used.
+         */
 
         Result onBlock(SolverContext& /*context*/, csdb::Pool& /*pool*/, const PublicKey& /*sender*/) override
         {
@@ -43,7 +75,7 @@ namespace slv2
 
     private:
 
-        /// <summary>   The pointer to own hash actual this round </summary>
+        /** @brief   The pointer to own hash actual this round */
         std::unique_ptr<Hash> pown;
 
     };
