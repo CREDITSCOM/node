6bc439f922a8eb9cc90d6d92a9985de84a74ae82
slavik <jcoder39@gmail.com>
2018-10-09 14:36:58 +0200

    tests: add dynamicbuffer 2

--- tests/csnode/dynamicbuffer_tests.cpp
+++ tests/csnode/dynamicbuffer_tests.cpp
@@ -1,11 +1,39 @@
 #define TESTING
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
+#include <algorithm>
+#include "dynamicbuffer.h"
 
-TEST(NodeTest, connection)
+using namespace cs;
+
+TEST(DynamicBuffer, equal_by_size)
 {
-MockConfig config;
-TransportMock transportMock();
+  DynamicBuffer a;
+  DynamicBuffer b(a);
 
-ASSERT_EQ(true, true);
+  ASSERT_TRUE(a == b);
+}
+
+TEST(DynamicBuffer, get_element_by_index)
+{
+  char str[] = "teststring";
+  size_t size = strlen(str) + 1;
+  DynamicBuffer a(size);
+  auto buffer = a.get();
+  strcpy(buffer, str);
+
+  ASSERT_TRUE(a[3] == str[3]);
+}
+
+TEST(DynamicBuffer, stl_interface)
+{
+  char str[] = "cba";
+  size_t size = strlen(str) + 1;
+  DynamicBuffer a(size);
+  auto buffer = a.get();
+  strcpy(buffer, str);
+
+  std::sort(a.begin(), a.end() - 1);
+
+  ASSERT_STREQ("abc", *a);
 }
