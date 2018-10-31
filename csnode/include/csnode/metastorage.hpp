8a94ab63c06511c865d576a8aac6d9d8d1d411c8
Arew <noobterr@yandex.ru>
2018-10-31 14:59:58 +0300

    Pop, get for meta storage added

--- csnode/include/csnode/metastorage.hpp
+++ csnode/include/csnode/metastorage.hpp
@@ -1,6 +1,7 @@
 #ifndef METASTORAGE_HPP
 #define METASTORAGE_HPP
 
+#include <optional>
 #include <lib/system/common.hpp>
 #include <boost/circular_buffer.hpp>
 
@@ -34,6 +35,9 @@ namespace cs
             }
         };
 
+        /// element type
+        using Element = MetaStorage<T>::MetaElement;
+
         // storage interface
 
         ///
@@ -42,6 +46,84 @@ namespace cs
         void resize(std::size_t size)
         {
             m_buffer.resize(size);
+        }
+
+        ///
+        /// @brief Appends meta element to buffer.
+        /// @param element Created from outside rvalue.
+        /// @return Returns true if append is success, otherwise returns false.
+        ///
+        bool append(MetaElement&& value)
+        {
+            const auto iterator = std::find(m_buffer.begin(), m_buffer.end(), value);
+            const auto result = (iterator == m_buffer.end());
+
+            if (result) {
+                m_buffer.push_back(std:move(value));
+            }
+
+            return result;
+        }
+
+        ///
+        /// @brief Appends meta element to buffer.
+        /// @param element Created from outside no need lvalue, cuz it would be moved.
+        /// @return Returns true if append is success, otherwise returns false.
+        ///
+        bool append(MetaElement& value)
+        {
+            return append(std::move(value));
+        }
+
+        ///
+        /// @brief Returns contains meta storage this round or not.
+        ///
+        bool contains(RoundNumber round)
+        {
+            const auto iterator = std::find_if(m_buffer.begin(), m_buffer.end(), [=](const MetaElement& value) {
+                return value.round == round;
+            });
+
+            return iterator != m_buffer.end();
+        }
+
+        ///
+        /// @brief Returns meta element if this round exists at storage, otherwise returns nothing.
+        /// @param round Round number to get element from storage.
+        /// @return Returns optional parameter.
+        ///
+        std::optional<MetaElement> get(RoundNumber round)
+        {
+            const auto iterator = std::find_if(m_buffer.begin(), m_buffer.end(), [=](const MetaElement& value) {
+                return value.round == round;
+            });
+
+            if (iterator != m_buffer.end()) {
+                return *iterator;
+            }
+
+            return std::nullopt;
+        }
+
+        ///
+        /// @brief Returns and removes meta element if it's exists at round parameter.
+        /// @param round Round number to get element from storage.
+        /// @return Returns meta element of storage if found, otherwise default constructed meta element.
+        ///.
+        MetaElement pop(RoundNumber round)
+        {
+            MetaElement result;
+
+            const auto iterator = std::find_if(m_buffer.begin(), m_buffer.end(), [=](const MetaElement& value) {
+                return value.round == round;
+            });
+
+            if (iterator != m_buffer.end()) {
+                result = std::move(*iterator);
+                m_buffer.erase(iterator);
+            }
+
+            return result;
         }
 
     private:
