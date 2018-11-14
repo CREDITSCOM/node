8a55522af77a83c451bed7a122b126a30de97b3a
i.azoyan <ivan.azoyan@gmail.com>
2018-11-14 16:49:24 +0300

    fix progress bar for Windows

--- lib/src/lib/system/progressbar.cpp
+++ lib/src/lib/system/progressbar.cpp
@@ -10,10 +10,7 @@ ProgressBar::ProgressBar(
 , barWidth(barWidthInSymbols)
 {
     rang::setControlMode(rang::control::Force);
-}
-
-ProgressBar::~ProgressBar() {
-    rang::setControlMode(rang::control::Auto);
+    rang::setWinTermMode(rang::winTerm::Ansi);
 }
 
 std::string ProgressBar::string(ProgressBar::Progress ticks) {
@@ -25,11 +22,16 @@ std::string ProgressBar::string(ProgressBar::Progress ticks) {
 
     std::stringstream result;
     result << rang::bg::blue << completed;
-    result << rang::bg::black << incompleted;
+    result << rang::bg::gray << incompleted;
     result << rang::bg::reset << rang::fg::reset << rang::style::reset;
     result << " " << (progress * 100.0f) << "%";
 
     result.flush();
     result << std::endl;
     return result.str();
+}
+
+ProgressBar::~ProgressBar() {
+    rang::setControlMode(rang::control::Auto);
+    rang::setWinTermMode(rang::winTerm::Auto);
 }
