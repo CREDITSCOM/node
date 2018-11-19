11b64f3b23e47a40f111e570bf8573a76b5fcff1
Sergey <sychev_sergey@inbox.ru>
2018-11-19 16:55:47 +0300

    endless spammer

--- solver/solver/include/solver/spammer.h
+++ solver/solver/include/solver/spammer.h
@@ -6,11 +6,13 @@
 #ifndef SOLVER_SPAMMER_H
 #define SOLVER_SPAMMER_H
 
+#include <array>
 #include <cstdint>
 #include <vector>
 #include <thread>
 
 #include <csdb/address.h>
+#include <csdb/internal/types.h>
 
 class Node;
 namespace csdb {
@@ -22,11 +24,9 @@ namespace cs {
 *  @brief This spammer creates transactions and
 *  sends them in a separate thread.
 *  
-*  Firstly, it generates own private and public
-*  key, and target public keys. Then it funds own
-*  public key with money from test address in genesis block
-*  and starts spam with transactions from own public key to
-*  target keys.
+*  Firstly, it generates own private and public keys.
+*  Then it funds own public keys with money from test address in genesis block
+*  and starts spam with transactions among own keys.
 *
 *  Signature verification for spammer transactions may not be switched off,
 *  because spammer signs each transaction. Also it fills source and target
@@ -35,26 +35,22 @@ namespace cs {
 class Spammer {
  public:
   void StartSpamming(Node&);
-  Spammer();
-  ~Spammer();
+  Spammer() = default;  
+  ~Spammer() = default;
 
   Spammer(const Spammer&) = delete;
   Spammer(Spammer&&) = delete;
   const Spammer& operator=(const Spammer&) = delete;
 
  private:
-  void GenerateTargetWallets();
+  void GenerateMyWallets();
   void SpamWithTransactions(Node&);
-  void FundMyWallet(Node&);
+  void FundMyWallets(Node&);
   csdb::Address OptimizeAddress(const csdb::Address&, Node&);
   void SignTransaction(csdb::Transaction&, const uint8_t* private_key);
 
-  // own public key from which spammer sends transactions
-  uint8_t* public_key_;
-  // own private key, which is used to sign transactions
-  uint8_t* private_key_;
   // wallets to which spammer sends transactions
-  std::vector<csdb::Address> target_wallets_;
+  std::vector<std::pair<csdb::Address, std::array<uint8_t, csdb::internal::kPrivateKeySize>>> my_wallets_;
   // thread for: void SpamWithTransactions(Node&)
   std::thread spam_thread_;
 };
