22e1dd994717ab23bc4d446c9f6c426a874bb87a
Sergey <sychev_sergey@inbox.ru>
2018-11-26 16:32:15 +0300

    cscrypto : generate key pair, generate signature, verify signature

--- crypto/include/cscrypto/cscrypto.hpp
+++ crypto/include/cscrypto/cscrypto.hpp
@@ -12,10 +12,20 @@ const size_t kPublicKeySize = 32;
 const size_t kPrivateKeySize = 64;
 const size_t kSignatureSize = 64;
 
-using PublicKey = std::array<uint8_t, kPublicKeySize>;
 using Hash = std::array<uint8_t, kHashSize>;
+using PublicKey = std::array<uint8_t, kPublicKeySize>;
+using PrivateKey = std::array<uint8_t, kPrivateKeySize>;
+using Signature = std::array<uint8_t, kSignatureSize>;
 
 void CalculateHash(Hash& hash, const uint8_t* data, size_t data_size);
+
+void GenerateKeyPair(PublicKey& public_key, PrivateKey& private_key);
+
+void GenerateSignature(Signature& signature, const PrivateKey& private_key,
+                       const uint8_t* data, size_t data_size);
+
+bool VerifySignature(const Signature& signature, const PublicKey& public_key,
+                     const uint8_t* data, size_t data_size);
 
 }  // namespace cscrypto
 
--- crypto/src/cscrypto.cpp
+++ crypto/src/cscrypto.cpp
@@ -1,11 +1,27 @@
 #include "cscrypto/cscrypto.hpp"
 
 #include <blake2.h>
+#include <sodium.h>
 
 namespace cscrypto {
 
 void CalculateHash(Hash& hash, const uint8_t* data, size_t data_size) {
   blake2sp(hash.data(), BLAKE2S_OUTBYTES, data, data_size, 0, 0);
+}
+
+void GenerateKeyPair(PublicKey& public_key, PrivateKey& private_key) {
+  crypto_sign_keypair(public_key.data(), private_key.data());
+}
+
+void GenerateSignature(Signature& signature, const PrivateKey& private_key,
+                       const uint8_t* data, size_t data_size) {
+  unsigned long long signature_len;
+  crypto_sign_ed25519_detached(signature.data(), &signature_len, data, data_size, private_key.data());
+}
+
+bool VerifySignature(const Signature& signature, const PublicKey& public_key,
+                     const uint8_t* data, size_t data_size) {
+  return !crypto_sign_ed25519_verify_detached(signature.data(), data, data_size, public_key.data());
 }
 
 }  // namespace cscrypto
