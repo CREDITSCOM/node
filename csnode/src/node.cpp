b6fb68b70ed8d440df8a976f48ebc6b4a966eb50
sokolvano <sokolvano@mail.ru>
2018-12-26 11:08:28 +0300

    Fix compile error

--- csnode/src/node.cpp
+++ csnode/src/node.cpp
@@ -2400,7 +2400,7 @@ std::string Node::getSenderText(const cs::PublicKey& sender) {
 
 void Node::spoileHash(const csdb::PoolHash& hashToSpoil, csdb::PoolHash& spoiledHash)
 {
-  csmeta(cslog)) << "begin";
+  csmeta(cslog) << "begin";
   cscrypto::Hash hash;
   cscrypto::CalculateHash(hash, hashToSpoil.to_binary().data(), sizeof(cs::Hash),(const cscrypto::Byte*) (roundNumber_), sizeof(cs::RoundNumber));
   cs::Bytes bytesHash(sizeof(cscrypto::Hash));
@@ -2409,7 +2409,7 @@ void Node::spoileHash(const csdb::PoolHash& hashToSpoil, csdb::PoolHash& spoiled
 }
 
 void Node::spoileHash(const csdb::PoolHash& hashToSpoil, cs::PublicKey pKey, csdb::PoolHash& spoiledHash) {
-  csmeta(cslog)) << "begin";
+  csmeta(cslog) << "begin";
   cscrypto::Hash hash;
   cscrypto::CalculateHash(hash, hashToSpoil.to_binary().data(), sizeof(cs::Hash), pKey.data(), sizeof(cs::PublicKey));
   cs::Bytes bytesHash(sizeof(cscrypto::Hash));
