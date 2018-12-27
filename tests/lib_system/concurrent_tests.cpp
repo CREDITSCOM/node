d6fea8e84090c8656a4fdbe0c56c89f998925343
Arew <noobterr@yandex.ru>
2018-12-27 12:30:31 +0300

    Concurrent tests added

---
+++ tests/lib_system/concurrent_tests.cpp
@@ -0,0 +1,44 @@
+#include "gtest/gtest.h"
+#include "lib/system/concurrent.hpp"
+
+#include <iostream>
+#include <string>
+#include <atomic>
+
+using ThreadId = std::thread::id;
+
+template<typename T>
+using Ref = std::reference_wrapper<T>;
+
+template<typename... Types>
+void print(Types&& ...args) {
+  (std::cout << ... << std::forward<Types>(args)) << std::endl;
+}
+
+TEST(Concurrent, SimpleRunWithBinding) {
+  std::atomic<bool> isRunningFinished = false;
+
+  ThreadId mainId = std::this_thread::get_id();
+  ThreadId concurrentId;
+
+  print("Main thread id: ", mainId);
+
+  class Demo {
+  public:
+    void method(const std::string& message, Ref<ThreadId> wrapper, Ref<std::atomic<bool>> finished) {
+      wrapper.get() = std::this_thread::get_id();
+
+      print("Concurrent thread id: ", wrapper);
+      print(message);
+
+      finished.get() = true;
+    }
+  };
+
+  Demo demo;
+  cs::Concurrent::run(std::bind(&Demo::method, &demo, "Finished", std::ref(concurrentId), std::ref(isRunningFinished)));
+
+  while(!isRunningFinished);
+
+  ASSERT_NE(mainId, concurrentId);
+}
