5fbac549621432120dd5c5ea61c1abae0c2026f1
Arew <noobterr@yandex.ru>
2018-12-27 16:33:21 +0300

    Add watcher tests to concurrent tests.

--- tests/lib_system/concurrent_tests.cpp
+++ tests/lib_system/concurrent_tests.cpp
@@ -15,13 +15,14 @@ void print(Types&& ...args) {
   (std::cout << ... << std::forward<Types>(args)) << std::endl;
 }
 
+#define GENERATE_THREAD_VALUES()\
+  std::atomic<bool> isRunningFinished = false;\
+  ThreadId mainId = std::this_thread::get_id();\
+  ThreadId concurrentId;\
+  print("Main thread id: ", mainId)
+
 TEST(Concurrent, SimpleRunWithBinding) {
-  std::atomic<bool> isRunningFinished = false;
-
-  ThreadId mainId = std::this_thread::get_id();
-  ThreadId concurrentId;
-
-  print("Main thread id: ", mainId);
+  GENERATE_THREAD_VALUES();
 
   class Demo {
   public:
@@ -44,12 +45,7 @@ TEST(Concurrent, SimpleRunWithBinding) {
 }
 
 TEST(Concurrent, SimpleRunWithoutBinding) {
-  std::atomic<bool> isRunningFinished = false;
-
-  ThreadId mainId = std::this_thread::get_id();
-  ThreadId concurrentId;
-
-  print("Main thread id: ", mainId);
+  GENERATE_THREAD_VALUES();
 
   class Demo {
   public:
@@ -72,16 +68,13 @@ TEST(Concurrent, SimpleRunWithoutBinding) {
 }
 
 TEST(Concurrent, SimpleRunLambda) {
-  std::atomic<bool> isRunningFinished = false;
-
-  ThreadId mainId = std::this_thread::get_id();
-  ThreadId concurrentId;
-
-  print("Main thread id: ", mainId);
+  GENERATE_THREAD_VALUES();
 
   auto lambda = [&] {
     concurrentId = std::this_thread::get_id();
+
     print("Concurrent thread id: ", concurrentId);
+    print("Finished");
 
     isRunningFinished = true;
   };
@@ -90,4 +83,75 @@ TEST(Concurrent, SimpleRunLambda) {
   while(!isRunningFinished);
 
   ASSERT_NE(mainId, concurrentId);
+}
+
+TEST(Concurrent, VoidFutureWatcherBindedRun) {
+  GENERATE_THREAD_VALUES();
+  static bool called = false;
+
+  class Demo {
+  public:
+    void method(const std::string& message, Ref<ThreadId> wrapper, Ref<std::atomic<bool>> finished) {
+      wrapper.get() = std::this_thread::get_id();
+
+      print("Concurrent thread id: ", wrapper);
+      print(message);
+
+      finished.get() = true;
+    }
+
+  public slots:
+    void onWatcherFinished() {
+      print("Watcher finished slot activated");
+      called = true;
+    }
+  };
+
+  Demo demo;
+  std::string message = "Finished";
+  auto binder = std::bind(&Demo::method, &demo, message, std::ref(concurrentId), std::ref(isRunningFinished));
+
+  cs::FutureWatcher<void> watcher = cs::Concurrent::run(cs::RunPolicy::ThreadPoolPolicy, std::move(binder));
+  cs::Connector::connect(&watcher.finished, &demo, &Demo::onWatcherFinished);
+
+  while(!isRunningFinished);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+  ASSERT_NE(mainId, concurrentId);
+  ASSERT_EQ(called, true);
+}
+
+TEST(Concurrent, VoidFutureWatcherNonBindedRun) {
+  GENERATE_THREAD_VALUES();
+  static bool called = false;
+
+  class Demo {
+  public:
+    void method(const std::string& message, Ref<ThreadId> wrapper, Ref<std::atomic<bool>> finished) {
+      wrapper.get() = std::this_thread::get_id();
+
+      print("Concurrent thread id: ", wrapper);
+      print(message);
+
+      finished.get() = true;
+    }
+
+  public slots:
+    void onWatcherFinished() {
+      print("Watcher finished slot activated");
+      called = true;
+    }
+  };
+
+  Demo demo;
+  std::string message = "Finished";
+
+  cs::FutureWatcher<void> watcher = cs::Concurrent::run(cs::RunPolicy::ThreadPoolPolicy, &Demo::method, &demo, message, std::ref(concurrentId), std::ref(isRunningFinished));
+  cs::Connector::connect(&watcher.finished, &demo, &Demo::onWatcherFinished);
+
+  while(!isRunningFinished);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+  ASSERT_NE(mainId, concurrentId);
+  ASSERT_EQ(called, true);
 }
