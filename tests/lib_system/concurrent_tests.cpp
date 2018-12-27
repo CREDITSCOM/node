1b85cf0f49b7c87d48831bf48dfb7663fac61700
Arew <noobterr@yandex.ru>
2018-12-27 12:49:48 +0300

    Add two additional tests for simple binded/non-binded runnable entity.

--- tests/lib_system/concurrent_tests.cpp
+++ tests/lib_system/concurrent_tests.cpp
@@ -42,3 +42,52 @@ TEST(Concurrent, SimpleRunWithBinding) {
 
   ASSERT_NE(mainId, concurrentId);
 }
+
+TEST(Concurrent, SimpleRunWithoutBinding) {
+  std::atomic<bool> isRunningFinished = false;
+
+  ThreadId mainId = std::this_thread::get_id();
+  ThreadId concurrentId;
+
+  print("Main thread id: ", mainId);
+
+  class Demo {
+  public:
+    void method(const std::string& message, Ref<ThreadId> wrapper, Ref<std::atomic<bool>> finished) {
+      wrapper.get() = std::this_thread::get_id();
+
+      print("Concurrent thread id: ", wrapper);
+      print(message);
+
+      finished.get() = true;
+    }
+  };
+
+  Demo demo;
+  cs::Concurrent::run(&Demo::method, &demo, "Finished", std::ref(concurrentId), std::ref(isRunningFinished));
+
+  while(!isRunningFinished);
+
+  ASSERT_NE(mainId, concurrentId);
+}
+
+TEST(Concurrent, SimpleRunLambda) {
+  std::atomic<bool> isRunningFinished = false;
+
+  ThreadId mainId = std::this_thread::get_id();
+  ThreadId concurrentId;
+
+  print("Main thread id: ", mainId);
+
+  auto lambda = [&] {
+    concurrentId = std::this_thread::get_id();
+    print("Concurrent thread id: ", concurrentId);
+
+    isRunningFinished = true;
+  };
+
+  cs::Concurrent::run(lambda);
+  while(!isRunningFinished);
+
+  ASSERT_NE(mainId, concurrentId);
+}
