85e2b7fa5c5df9cf5dd940c3751a3597ff032717
Alexander Avramenko <aav@credits.com>
2019-01-29 10:31:25 +0300

    Fix big bang problem after node is restarted

--- solver/include/solver/solvercore.hpp
+++ solver/include/solver/solvercore.hpp
@@ -31,6 +31,8 @@ class SmartContracts;
 
 namespace cs {
 
+  constexpr cs::Byte InvalidConfidant = 255;
+
 class SolverCore {
 public:
   using Counter = size_t;
@@ -297,9 +299,9 @@ private:
   std::vector<cs::StageOneSmarts> smartStageOneStorage_;
   std::vector<cs::StageTwoSmarts> smartStageTwoStorage_;
   std::vector<cs::StageThreeSmarts> smartStageThreeStorage_;
-  bool smartStagesStorageRefreshed_ = false;
+  bool smartStagesStorageRefreshed_;
   std::vector<cs::PublicKey> smartConfidants_;
-  uint8_t ownSmartsConfNum_ = 255;
+  uint8_t ownSmartsConfNum_;
   cs::Sequence smartRoundNumber_;
   cs::TransactionsPacket currentSmartTransactionPack_;
   cs::StageOneSmarts st1;
