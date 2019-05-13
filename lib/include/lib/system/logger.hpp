c70feaff994e99673aa4da896d1957517fc69c2e
0xAAE <avramenko.a@gmail.com>
2019-05-14 00:23:58 +0300

    Add formatter for numbers to print to log with delimiters

--- lib/include/lib/system/logger.hpp
+++ lib/include/lib/system/logger.hpp
@@ -8,6 +8,8 @@
 #include <boost/log/utility/manipulators/dump.hpp>
 #include <boost/log/utility/setup/settings.hpp>
 
+#include <sstream>
+
 /*
  * \brief Just a syntax shugar over Boost::Log v2.
  * So you can use all the power of boost \link https://www.boost.org/doc/libs/1_67_0/libs/log/doc/html/index.html
@@ -81,5 +83,41 @@ BOOST_LOG_INLINE_GLOBAL_LOGGER_CTOR_ARGS(File, logging::sources::severity_channe
 
 // alias
 #define cslog(...) csinfo(__VA_ARGS__)
+
+template <typename N>
+class WithDelimiters
+{
+public:
+
+    WithDelimiters(N value) {
+        val_ = value;
+    }
+
+    std::string to_string() {
+        std::ostringstream os;
+        // the locale is responsible for calling the matching delete from its own destructor for facet arg
+        os.imbue(std::locale(std::locale::classic(), new WithDelimiters::NumFormatter()));
+        os << val_;
+        return os.str();
+    }
+
+private:
+    N val_;
+
+    class NumFormatter : public std::numpunct<char>
+    {
+    protected:
+        virtual char do_thousands_sep() const { return '\''; }
+        virtual std::string do_grouping() const { return "\03"; }
+    };
+
+};
+
+template <typename N>
+std::ostream& operator<<(std::ostream& os, WithDelimiters<N> value) {
+    os << value.to_string();
+    return os;
+}
+
 
 #endif  // LOGGER_HPP
