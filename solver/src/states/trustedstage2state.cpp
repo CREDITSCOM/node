05bc9848113c39f484a2548695a6612f2f37583f
Alexander Avramenko <aav@credits.com>
2019-07-22 19:51:12 +0300

    Fix empty timestsmp in stage-1 issue

--- solver/src/states/trustedstage2state.cpp
+++ solver/src/states/trustedstage2state.cpp
@@ -110,8 +110,9 @@ Result TrustedStage2State::onStage1(SolverContext& context, const cs::StageOne& 
         csdebug() << name() << ": no valid last block - can't get timestamp";
         return Result::Ignore;
     }
-    auto myStamp = std::stoll(context.stage1(context.own_conf_number())->roundTimeStamp);
-    auto otherStamp = std::stoll(st.roundTimeStamp);
+    const std::string ts = context.stage1(context.own_conf_number())->roundTimeStamp;
+    uint64_t myStamp = ts.empty() ? 0 : std::stoll(ts);
+    uint64_t otherStamp = st.roundTimeStamp.empty() ? 0 : (uint64_t) std::stoll(st.roundTimeStamp);
     if (otherStamp < context.lastTimeStamp() || otherStamp > myStamp + Consensus::DefaultTimeStampRange * 2) {
         csdebug() << name() << ": [" << static_cast<int>(st.sender) << "] mark untrusted: invalid timeStamp";
         context.mark_untrusted(st.sender);
