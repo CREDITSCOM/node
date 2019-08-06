31d4311926a31ac2c5555dca6906d2a7e227b701
Arew <noobterr@yandex.ru>
2019-08-06 18:05:41 +0300

    Change benchmark for any callable type, add testfolder for correct benchmark instantiation, add lmdb bench

--- benchmark/include/benchmark.hpp
+++ benchmark/include/benchmark.hpp
@@ -12,32 +12,37 @@
 #define forever for(;;)
 
 namespace cs {
-class Benchmark {
-public:
+class BenchmarkBase {
+protected:
     using Duration = decltype(std::chrono::steady_clock::now() - std::chrono::steady_clock::now());
-    using Future = std::future<std::pair<Duration, bool>>;
-    using FuturePtr = std::shared_ptr<Future>;
 
-    template <typename Func>
-    static bool run(Func f, std::chrono::seconds max = std::chrono::seconds(10)) {
+    static void started() {
         cs::Console::writeLine("\nStart benchmark");
+    }
 
-        auto lambda = [=] {
-            auto now = std::chrono::steady_clock::now();
-            auto result = f();
+    template<typename T, typename Future>
+    static void finished(const T& difference, Future& future) {
+        static const char* title = "Benchmark finished, time in";
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(difference);
 
-            auto difference = std::chrono::steady_clock::now() - now;
-            return std::make_pair(difference, result);
-        };
+        if (duration.count() != 0) {
+            cs::Console::writeLine(title, " ms ", duration.count());
+        }
+        else {
+            cs::Console::writeLine(title, " ns ", std::chrono::duration_cast<std::chrono::nanoseconds>(difference).count());
+        }
 
-        future_ = std::make_shared<Future>(std::async(std::launch::async, lambda));
+        future = Future{};
+    }
 
+    template <typename T, typename K>
+    static void monitor(std::shared_ptr<T> future, const K& max) {
         auto now = std::chrono::steady_clock::now();
         std::chrono::milliseconds ms(0);
 
         forever {
             static const std::chrono::milliseconds waitTime(10);
-            std::future_status status = future_->wait_for(waitTime);
+            std::future_status status = future->wait_for(waitTime);
 
             if (status == std::future_status::ready) {
                 break;
@@ -53,20 +58,65 @@ public:
                 }
             }
         }
+    }
+};
+
+template<typename T>
+class Benchmark : public BenchmarkBase {
+    using Duration = typename BenchmarkBase::Duration;
+    using Future = std::future<std::pair<Duration, T>>;
+    using FuturePtr = std::shared_ptr<Future>;
+
+public:
+    template <typename Func>
+    static T run(Func f, std::chrono::seconds max = std::chrono::seconds(10)) {
+        BenchmarkBase::started();
+
+        auto lambda = [=] {
+            auto now = std::chrono::steady_clock::now();
+            auto result = f();
+
+            auto difference = std::chrono::steady_clock::now() - now;
+            return std::make_pair(difference, result);
+        };
+
+        future_ = std::make_shared<Future>(std::async(std::launch::async, lambda));
+        BenchmarkBase::monitor(future_, max);
 
         const auto [difference, result] = future_->get();
-        static const char* title = "Benchmark finished, time in";
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(difference);
+        BenchmarkBase::finished(difference, future_);
 
-        if (duration.count() != 0) {
-            cs::Console::writeLine(title, " ms ", duration.count());
-        }
-        else {
-            cs::Console::writeLine(title, " ns ", std::chrono::duration_cast<std::chrono::nanoseconds>(difference).count());
-        }
-
-        future_ = FuturePtr();
         return result;
+    }
+
+private:
+    inline static FuturePtr future_;
+};
+
+template <>
+class Benchmark<void> : public BenchmarkBase {
+    using Duration = typename BenchmarkBase::Duration;
+    using Future = std::future<Duration>;
+    using FuturePtr = std::shared_ptr<Future>;
+
+public:
+    template <typename Func>
+    static void run(Func f, std::chrono::seconds max = std::chrono::seconds(10)) {
+        static_assert (std::is_same_v<std::invoke_result_t<Func>, void>, "Benchmark<void> should use only functors with void result type");
+
+        BenchmarkBase::started();
+
+        auto lambda = [=] {
+            auto now = std::chrono::steady_clock::now();
+            f();
+            return std::chrono::steady_clock::now() - now;
+        };
+
+        future_ = std::make_shared<Future>(std::async(std::launch::async, lambda));
+        BenchmarkBase::monitor(future_, max);
+
+        const auto difference = future_->get();
+        BenchmarkBase::finished(difference, future_);
     }
 
 private:
--- benchmark/include/framework.hpp
+++ benchmark/include/framework.hpp
@@ -1,28 +1,39 @@
 #ifndef FRAMEWORK_HPP
 #define FRAMEWORK_HPP
 
+#include <type_traits>
 #include <benchmark.hpp>
 
 namespace cs {
 class Framework {
 public:
-    template <typename Func>
-    static void execute(Func func, const std::string& failedMessage = std::string{}, size_t count = 1) {
-        for (size_t i = 0; i < count; ++i) {
-            try {
-                if (!cs::Benchmark::run(func)) {
-                    cs::Console::writeLine("\n", failedMessage, " on ", i + 1, " iteration");
+    template <typename Func, typename Result = std::invoke_result_t<Func>>
+    static Result execute(Func func, std::chrono::seconds max = std::chrono::seconds(30), [[maybe_unused]] const std::string& failedMessage = std::string{}) {
+        try {
+            if constexpr (std::is_same_v<Result, bool>) {
+                auto result = cs::Benchmark<bool>::run(func, max);
+
+                if (!result) {
+                    cs::Console::writeLine("\n", failedMessage);
                     cs::Console::writeLine("Failed running other tests, fix previous test to correct execution");
 
                     std::exit(0);
                 }
-            }
-            catch (const std::exception& e) {
-                cs::Console::writeLine("Exception catched, message: ", e.what());
-                cs::Console::writeLine("Benchmark would be terminated");
 
-                std::terminate();
+                return result;
             }
+            else if constexpr (std::is_same_v<Result, void>) {
+                cs::Benchmark<void>::run(func, max);
+            }
+            else {
+                return cs::Benchmark<Result>::run(func, max);
+            }
+        }
+        catch (const std::exception& e) {
+            cs::Console::writeLine("Exception catched, message: ", e.what());
+            cs::Console::writeLine("Benchmark would be terminated");
+
+            std::terminate();
         }
     }
 };
