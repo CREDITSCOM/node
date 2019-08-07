25c9583793411f734dd7d013da8357cb4cf7eb17
Arew <noobterr@yandex.ru>
2019-08-07 17:10:09 +0300

    Fix benchmark for lmdb env flags test, add tests behaviours

--- benchmark/lmdbbench/main.cpp
+++ benchmark/lmdbbench/main.cpp
@@ -8,16 +8,16 @@ static void runBench(cs::Lmdb* db) {
     std::string key = "Key";
     std::string value = "Value";
 
-    for (size_t i = 0; i < 1000; ++i) {
+    for (size_t i = 0; i < 10000; ++i) {
         db->insert(key + std::to_string(i), value);
     }
 }
 
-static void testLmdbDefaultFlags() {
+static void testLmdb(unsigned int flags) {
     const char* path = "testdbpath";
 
     cs::Lmdb db(path);
-    db.open();
+    db.open(flags);
 
     cs::Framework::execute(std::bind(&runBench, &db), std::chrono::seconds(100), "Db run failed");
 
@@ -25,7 +25,17 @@ static void testLmdbDefaultFlags() {
     fs::remove_all(fs::path(path));
 }
 
+static void testLmdbDefaultFlags() {
+    testLmdb(lmdb::env::default_flags);
+}
+
+static void testLmdbWithFlags() {
+    testLmdb(MDB_NOSYNC | MDB_WRITEMAP | MDB_MAPASYNC);
+}
+
 int main() {
     testLmdbDefaultFlags();
+    testLmdbWithFlags();
+
     return 0;
 }
