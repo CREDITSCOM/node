6dd6ff915ee1727aca4ce482bcbce283a7052d61
Arew <noobterr@yandex.ru>
2019-09-19 17:11:15 +0300

    Compressor decode base58 test added

--- tests/csnode/compressor_tests.cpp
+++ tests/csnode/compressor_tests.cpp
@@ -4,6 +4,11 @@
 
 #include <compressor.hpp>
 
+#include <lib/system/utils.hpp>
+#include <lib/system/console.hpp>
+
+#include <cscrypto/cscrypto.hpp>
+
 TEST(Compressor, BaseUsage) {
     cs::Compressor compressor;
 
@@ -18,4 +23,25 @@ TEST(Compressor, BaseUsage) {
     auto entity = compressor.decompress<std::vector<size_t>>(ptr);
 
     ASSERT_EQ(data, entity);
+}
+
+TEST(Compressor, TestCompressBase58) {
+    cs::Compressor compressor;
+    cs::PublicKey key{};
+    std::initializer_list<char> list{ '4', 't', 'E', 'Q', 'b', 'Q', 'P', 'Y', 'Z', 'q', '1', 'b', 'Z', '8', 'T', 'n', '9', 'D', 'p', 'C', 'X', 'Y',
+                                      'U', 'g', 'P', 'g', 'E', 'g', 'c', 'q', 's', 'B', 'P', 'X', 'X', '4', 'f', 'X', 'e', 'f', '7', 'F', 'u', 'L' };
+
+    std::string base58 { list };
+    std::vector<unsigned char> decoded;
+
+    if (!DecodeBase58(base58, decoded)) {
+        cs::Console::writeLine("Can not decode base58 str");
+    }
+
+    std::copy(std::begin(decoded), std::end(decoded), std::begin(key));
+
+    auto ptr = compressor.compress(key);
+    auto entity = compressor.decompress<cs::PublicKey>(ptr);
+
+    ASSERT_EQ(key, entity);
 }
