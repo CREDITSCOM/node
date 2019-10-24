8b070ef8bd024bf924e25c68fb818b4c20eca758
Alexander Avramenko <aav@credits.com>
2019-10-24 11:56:05 +0300

    Creditsnet-4.2.432.0

--- client/config/config.cpp
+++ client/config/config.cpp
@@ -28,7 +28,7 @@
 #include <unistd.h>
 #endif
 
-const NodeVersion NODE_VERSION = 435;
+const NodeVersion NODE_VERSION = 432;
 
 const std::string BLOCK_NAME_PARAMS = "params";
 const std::string BLOCK_NAME_SIGNAL_SERVER = "signal_server";
