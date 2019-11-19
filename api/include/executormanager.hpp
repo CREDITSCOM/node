cbe3429d5e2d55a51076144bdae0345379fb9314
Arew <noobterr@yandex.ru>
2019-11-19 10:22:52 +0300

    ExecutorManager supports multiple executors control.

--- api/include/executormanager.hpp
+++ api/include/executormanager.hpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 #include <optional>
 
 #ifdef _MSC_VER
@@ -14,17 +15,24 @@
 namespace cs {
 class ExecutorManager {
 public:
+    using ProcessId = boost::process::pid_t;
+
     // returns true if executor exists at java processes
+    bool isExecutorProcessRunning(ProcessId id) const;
     bool isExecutorProcessRunning() const;
 
-    // returns executor pid if executor exists at java processes
-    std::optional<boost::process::pid_t> executorProcessPid() const;
+    // returns all executor pids if executor exists at java processes
+    std::optional<std::vector<ProcessId>> executorProcessIds() const;
 
-    // stops executor, returns false if executor does not exist
+    // stops executor
+    bool stopExecutorProcess(ProcessId id);
+
+    // stops all executor processes
     bool stopExecutorProcess();
 
 private:
     std::string jpsData() const;
+    void terminate(boost::process::pid_t pid);
 
     const std::string jpsName_ = "jps";
     const std::string executorName_ = "contract-executor";
