15fce6031ea95cffc941a63156b21d9a2760260d
Arew <noobterr@yandex.ru>
2020-01-21 14:01:57 +0300

    Fix observer public and private key caching.

--- client/config/config.hpp
+++ client/config/config.hpp
@@ -129,6 +129,10 @@ struct DbSQLData {
     std::string password { "postgres" };
 };
 
+namespace cs::config {
+class Observer;
+}
+
 class Config {
 public:
     Config() = default;
@@ -352,6 +356,7 @@ private:
     EventsReportData eventsReport_;
 
     friend bool operator==(const Config&, const Config&);
+    friend class cs::config::Observer;
 };
 
 // all operators
