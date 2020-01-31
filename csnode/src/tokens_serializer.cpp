c1b57c1c415284d188611ea9a62ab6c790ff17f7
Vladimir Shilkin <vshilkin@gmail.com>
2020-01-31 13:37:15 +0300

    csnode: tokens serialization

---
+++ csnode/src/tokens_serializer.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#include <tokens.hpp>
+#include <csnode/tokens_serializer.hpp>
+
+namespace cs {
+void TokensMaster_Serializer::bind(TokensMaster& tokens) {
+    tokens_ = reinterpret_cast<decltype(tokens_)>(&tokens.tokens_);
+    holders_ = reinterpret_cast<decltype(holders_)>(&tokens.holders_);
+}
+
+void TokensMaster_Serializer::save() {
+    std::ofstream ofs("tokens.dat");
+    boost::archive::text_oarchive oa(ofs);
+    oa << *tokens_;
+    oa << *holders_;
+}
+
+void TokensMaster_Serializer::load() {
+    std::ifstream ifs("tokens.dat");
+    boost::archive::text_iarchive ia(ifs);
+    ia >> *tokens_;
+    ia >> *holders_;
+}
+}  // namespace cs
