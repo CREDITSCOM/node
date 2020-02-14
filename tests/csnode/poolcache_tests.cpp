b277ac57b718fe1a6cafab6f4bb042ecd1c19958
Arew <noobterr@yandex.ru>
2020-02-14 17:57:58 +0300

    Fix poolcache tests. Passed Ok.

--- tests/csnode/poolcache_tests.cpp
+++ tests/csnode/poolcache_tests.cpp
@@ -20,12 +20,24 @@ struct CacheDeleter {
 using PoolCachePtr = std::unique_ptr<cs::PoolCache, CacheDeleter>;
 
 static PoolCachePtr createPoolCache() {
+    cs::FileUtils::createPathIfNoExist(dbPath);
     return std::unique_ptr<cs::PoolCache, CacheDeleter>(new cs::PoolCache(dbPath));
 }
 
 TEST(PoolCache, Insert) {
+    const size_t value = 10;
     auto cache = createPoolCache();
-    cache->insert(10, {0});
+    cache->insert(value, {0});
 
-    ASSERT_TRUE(cache->contains(10));
+    ASSERT_TRUE(cache->contains(value));
+}
+
+TEST(PoolCache, Remove) {
+    const size_t value = 10;
+    auto cache = createPoolCache();
+    cache->insert(value, {0});
+
+    ASSERT_TRUE(cache->contains(value));
+    ASSERT_TRUE(cache->remove(value));
+    ASSERT_FALSE(cache->contains(value));
 }
