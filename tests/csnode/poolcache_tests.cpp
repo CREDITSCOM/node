581ccfca3d180e82a5e37f861490beedb98434db
Arew <noobterr@yandex.ru>
2020-02-17 15:01:17 +0300

    Fix pool cache tests

--- tests/csnode/poolcache_tests.cpp
+++ tests/csnode/poolcache_tests.cpp
@@ -8,6 +8,10 @@
 
 #include <csnode/poolcache.hpp>
 
+#include <csdb/currency.hpp>
+#include <csdb/amount.hpp>
+#include <csdb/amount_commission.hpp>
+
 static const std::string dbPath = "./temppoolcaches";
 
 struct CacheDeleter {
@@ -24,10 +28,25 @@ static PoolCachePtr createPoolCache() {
     return std::unique_ptr<cs::PoolCache, CacheDeleter>(new cs::PoolCache(dbPath));
 }
 
+static csdb::Transaction createTestTransaction(const int64_t id, const uint8_t amount) {
+    cs::Signature sign{};
+    csdb::Transaction transaction{id,
+                                  csdb::Address::from_public_key(cs::PublicKey{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                                                                               0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01}),
+                                  csdb::Address::from_public_key(cs::PublicKey{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                                                                               0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02}),
+                                  csdb::Currency{amount},
+                                  csdb::Amount{0, 0},
+                                  csdb::AmountCommission{0.},
+                                  csdb::AmountCommission{0.},
+                                  sign};
+    return transaction;
+}
+
 TEST(PoolCache, Insert) {
     const size_t value = 10;
     auto cache = createPoolCache();
-    cache->insert(value, {0});
+    cache->insert(value, {0}, cs::PoolStoreType::Created);
 
     ASSERT_TRUE(cache->contains(value));
 }
@@ -35,9 +54,41 @@ TEST(PoolCache, Insert) {
 TEST(PoolCache, Remove) {
     const size_t value = 10;
     auto cache = createPoolCache();
-    cache->insert(value, {0});
+    cache->insert(value, {0}, cs::PoolStoreType::Created);
 
     ASSERT_TRUE(cache->contains(value));
     ASSERT_TRUE(cache->remove(value));
     ASSERT_FALSE(cache->contains(value));
+}
+
+TEST(PoolCache, TestCount) {
+    auto cache = createPoolCache();
+    cache->insert(1, {0}, cs::PoolStoreType::Created);
+    cache->insert(2, {0}, cs::PoolStoreType::Created);
+    cache->insert(3, {0}, cs::PoolStoreType::Synced);
+    cache->insert(4, {0}, cs::PoolStoreType::Synced);
+    cache->insert(5, {0}, cs::PoolStoreType::Synced);
+
+    ASSERT_EQ(cache->size(), 5);
+    ASSERT_EQ(cache->sizeSynced(), 3);
+    ASSERT_EQ(cache->sizeCreated(), 2);
+}
+
+TEST(PoolCache, TestPoolCorrectSerialization) {
+    auto cache = createPoolCache();
+    csdb::Pool pool;
+
+    pool.set_sequence(10);
+    pool.add_transaction(createTestTransaction(1, 100));
+    pool.compose();
+
+    cache->insert(1, pool.to_binary(), cs::PoolStoreType::Created);
+
+    auto [p, type] = cache->pop(1);
+    ASSERT_EQ(cs::PoolStoreType::Created, type);
+
+    ASSERT_EQ(pool.sequence(), p.sequence());
+    ASSERT_EQ(pool.transactions().front().to_binary(), p.transactions().front().to_binary());
+
+    ASSERT_TRUE(cache->isEmpty());
 }
