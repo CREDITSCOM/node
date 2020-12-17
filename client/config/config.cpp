a047a15e728109f40e189e42b6deadd65c344338
cdmitry77 <cdmitry77@gmail.com>
2020-12-17 14:56:31 +0300

    version change to 525

--- client/config/config.cpp
+++ client/config/config.cpp
@@ -27,7 +27,7 @@
 #include <unistd.h>
 #endif
 
-const NodeVersion NODE_VERSION = 523;
+const NodeVersion NODE_VERSION = 525;
 
 const std::string BLOCK_NAME_PARAMS = "params";
 const std::string BLOCK_NAME_HOST_INPUT = "host_input";
