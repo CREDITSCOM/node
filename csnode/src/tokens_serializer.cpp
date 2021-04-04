775cdb48247099f27c8e13cde1d4ab99372d3ed2
Sergey Sychev <sychev_sergey@inbox.ru>
2021-04-04 12:18:43 +0300

    [CP-181]: TokesMaster_Serializer hash

--- csnode/src/tokens_serializer.cpp
+++ csnode/src/tokens_serializer.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <sstream>
 
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
@@ -23,6 +24,20 @@ void TokensMaster_Serializer::save() {
     boost::archive::text_oarchive oa(ofs);
     oa << *tokens_;
     oa << *holders_;
+}
+
+::cscrypto::Hash TokensMaster_Serializer::hash() {
+    std::ostringstream ofs;
+    {
+      boost::archive::text_oarchive oa(ofs);
+      oa << *tokens_;
+      oa << *holders_;
+    }
+    auto data = ofs.str();
+    return ::cscrypto::calculateHash(
+        (const ::cscrypto::Byte*)data.data(),
+        data.size()
+    );
 }
 
 void TokensMaster_Serializer::load() {
