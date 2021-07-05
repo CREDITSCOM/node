418b53d5dd917bf21adc2721d4f604baa335700d
Sergey Sychev <sychev_sergey@inbox.ru>
2021-07-05 15:43:41 +0300

    service: unix service partial impl

--- lib/include/lib/system/service/service_owner.hpp
+++ lib/include/lib/system/service/service_owner.hpp
@@ -8,27 +8,27 @@ namespace cs {
 
 class ServiceOwner {
 public:
-  virtual bool onInit(const char*) { return true; }
-  virtual bool onRun(const char*) = 0;
-  virtual bool onStop() = 0;
+    virtual bool onInit(const char*) { return true; }
+    virtual bool onRun(const char*) = 0;
+    virtual bool onStop() = 0;
 #ifndef _WIN32
-  virtual bool onFork(const char*, pid_t) { return true; }
+    virtual bool onFork(const char*, pid_t) { return true; }
 #endif // !_WIN32
-  virtual bool onPause() { return true; }
-  virtual bool onContinue() { return true; }
-  virtual bool onParamChange() { return true; }
-  virtual bool onException() { return true; }
+    virtual bool onPause() { return true; }
+    virtual bool onContinue() { return true; }
+    virtual bool onParamChange() { return true; }
+    virtual bool onException() { return true; }
 #ifdef WIN32
 #if (_WIN32_WINNT >= _WIN32_WINNT_WINXP)
-  virtual bool onSessionChanged(int what, const char* info) { return true; }
-  virtual bool onDeviceEvent(int what, const char* info) { return true; }
+    virtual bool onSessionChanged(int what, const char* info) { return true; }
+    virtual bool onDeviceEvent(int what, const char* info) { return true; }
 #endif // _WIN32_WINNT >= _WIN32_WINNT_WINXP
 #if (_WIN32_WINNT >= _WIN32_WINNT_VISTA)
-  virtual bool onPreshutdown() { return true; }
+    virtual bool onPreshutdown() { return true; }
 #endif // _WIN32_WINNT >= _WIN32_WINNT_VISTA
 #endif // WIN32
 
-  virtual ~ServiceOwner() = default;
+    virtual ~ServiceOwner() = default;
 };
 
 } // namespace cs
--- lib/include/lib/system/service/unix_service.hpp
+++ lib/include/lib/system/service/unix_service.hpp
@@ -1,9 +1,131 @@
 #pragma once
 
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
+#include <pthread.h>
+#include <signal.h>
+
+#include "service_owner.hpp"
+
 namespace cs {
 
 class Service {
+public:
+    Service(ServiceOwner&, const char* serviceName = nullptr);
 
+    bool run();
+
+private:
+    using mutex_type = std::mutex;
+    using lock_type = std::unique_lock<mutex_type>;
+    using cond_var_type = std::condition_variable;
+    using thread_type = std::thread;
+
+    struct ThreadsStatus {
+        bool signalReady = false;
+        bool workerReady = false;
+        bool mainReady = false;
+        bool error = false;
+    };
+
+    void waitForSignals();
+    void doWork();
+    bool startSignalThread();
+    bool startWorkerThread();
+
+    ServiceOwner& owner_;
+    const char* serviceName_;
+
+    thread_type signalThread_;
+    thread_type workerThread_;
+
+    mutex_type mux_;
+    cond_var_type cv_;
+
+    ThreadsStatus threadsStatus_;
 };
+
+inline Service::Service(ServiceOwner& owner, const char* serviceName)
+    : owner_(owner), serviceName_(serviceName) {}
+
+inline bool Service::run() {
+    auto pid = ::fork();
+#ifndef DISABLE_DAEMON
+    switch (pid) {
+        // child
+        case 0 :
+            if (!owner_.onFork(serviceName_, pid)) return false;
+            break;
+        // exit from parent or error
+        default :
+            return owner_.onFork(serviceName_, pid);
+    }
+#endif // !DISABLE_DAEMON
+    sigset_t signals;
+    sigfillset(&signals);
+    if (pthread_sigmask(SIG_BLOCK, &signals, nullptr) != 0) {
+        return false;
+    }
+
+    bool result = true;
+
+    try {
+        if (!startSignalThread() || !startWorkerThread()) {
+            result = false;
+            lock_type lock(mux_);
+            threadsStatus_.mainReady = true;
+            cv_.notify_all();
+        }
+        else {
+            
+        }
+    }
+    catch (...) {
+        result &= owner_.onException();
+    }
+
+    if (signalThread_.joinable()) {
+        signalThread_.join();
+    }
+    if (workerThread_.joinable()) {
+        workerThread_.join();
+    }
+
+    return result;
+}
+
+inline void Service::waitForSignals() {
+
+}
+
+inline void Service::doWork() {
+
+}
+
+inline bool Service::startSignalThread() {
+    signalThread_ = thread_type(&Service::waitForSignals, this);
+
+    lock_type lock(mux_);
+    cv_.wait(lock, [this]() { return threadsStatus_.signalReady; });
+    threadsStatus_.signalReady = false;
+    bool error = threadsStatus_.error;
+    threadsStatus_.error = false;
+
+    return !error;
+}
+
+inline bool Service::startWorkerThread() {
+    workerThread_ = thread_type(&Service::doWork, this);
+
+    lock_type lock(mux_);
+    cv_.wait(lock, [this]() { return threadsStatus_.workerReady; });
+    threadsStatus_.workerReady = false;
+    bool error = threadsStatus_.error;
+    threadsStatus_.error = false;
+
+    return !error;
+}
 
 } // namespace cs
